df06dd392a967fd94522a6069667a55c95de7a6e
Michael Metdepenningen <michael@sevendays.be>
2023-10-09 12:55:46 +0200

    add UserResource with role enum select

--- app/Livewire/DropdownTest.php
+++ app/Livewire/DropdownTest.php
@@ -3,15 +3,12 @@
 namespace App\Livewire;
 
 use App\Models\User;
-use Filament\Forms\Components\Select;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
-use Filament\Forms\Form;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
-use Livewire\Attributes\Url;
 use Livewire\Component;
 
 class DropdownTest extends Component implements HasForms, HasTable
@@ -19,46 +16,6 @@ class DropdownTest extends Component implements HasForms, HasTable
     use InteractsWithForms;
     use InteractsWithTable;
 
-    #[Url]
-    public ?string $formValue1 = '';
-    #[Url]
-    public ?string $formValue2 = '';
-
-    public function form(Form $form): Form
-    {
-        return $form->schema([
-
-            Select::make('formValue1')
-                ->options(function () {
-                    if ($this->formValue2 != '') {
-                        return [
-                            '1' => '1',
-                            '2' => '2',
-                        ];
-                    }
-                    return [
-                        '1a' => 'do not select 1',
-                        '1b' => 'do not select 2',
-                        '1c' => 'do not select 3',
-                        '1' => '1',
-                        '2' => '2',
-                    ];
-                })
-                ->reactive()
-            ,
-            Select::make('formValue2')
-                ->options(function () {
-                    return [
-                        'value 1' => 'Value 1',
-                        'value 2' => 'Value 2',
-                        'value 3' => 'Value 3',
-                        'value 4' => 'Value 4',
-                        'value 5' => 'Value 5',
-                    ];
-                })
-                ->reactive(),
-        ]);
-    }
 
     public function table(Table $table): Table
     {
@@ -67,8 +24,10 @@ class DropdownTest extends Component implements HasForms, HasTable
                 return User::query();
             })
             ->columns([
-                TextColumn::make('email')
-            ]);
+                TextColumn::make('email')->sortable()
+            ])
+            ->defaultSort('email', 'asc')
+            ;
     }
 
     public function render()
